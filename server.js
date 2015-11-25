907bc6a8300206b83f037243fb59a5dc7b2aebd3
Ed Solis <edsfocci@gmail.com>
2015-11-25 17:26:05 -0600

    Prepare to add to Heroku

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('static'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/base.html'));
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
